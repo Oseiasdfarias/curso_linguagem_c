3d88e237c5441f34dd2f2dce5b4701aabafc17d3
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-05-22 12:58:47 -0300

    atualizacao pasta ponteiro, stript ponteiro generico

---
+++ ponteiro/ponteiro_generico.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	/* Ponteiro genérico */
+	void *pp;
+	int *p1, p2 = 19;
+
+	p1 = &p2;
+	pp = &p2; // Endereço de int
+	printf("Endereço em pp: %p\n", pp);
+	printf("Endereço em p1: %p\n\n", p1);
+	printf("Conteudo pp: %d\n", *(int*)pp);
+	printf("Conteudo p1: %d\n\n", *p1);
+
+	pp = &p1; // Endereço de int*
+	printf("Endereço em pp: %p\n", pp);
+	printf("Endereço de p1: %p\n\n", &p1);
+
+	pp = p1; // Endereço de int
+	printf("Endereço em pp: %p\n", pp);
+	printf("Endereço de p1: %p\n\n", &p2);
+
+	return 0;
+}
