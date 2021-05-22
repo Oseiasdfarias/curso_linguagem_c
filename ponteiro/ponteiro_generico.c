28d7bebf522ec36b943a1a00d7436ba25cc443ef
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-05-22 17:41:55 -0300

    atualização Arquivos pasta Ponteiro

--- ponteiro/ponteiro_generico.c
+++ ponteiro/ponteiro_generico.c
@@ -3,7 +3,7 @@
 int main(){
 	/* Ponteiro genérico */
 	void *pp;
-	int *p1, p2 = 19;
+	int *p1, p2 = 32;
 
 	p1 = &p2;
 	pp = &p2; // Endereço de int
@@ -15,6 +15,9 @@ int main(){
 	pp = &p1; // Endereço de int*
 	printf("Endereço em pp: %p\n", pp);
 	printf("Endereço de p1: %p\n\n", &p1);
+	printf("Conteudo pp: %d\n", *(int*)pp);
+	printf("Conteudo p1: %d\n\n", *p1);
+
 
 	pp = p1; // Endereço de int
 	printf("Endereço em pp: %p\n", pp);
