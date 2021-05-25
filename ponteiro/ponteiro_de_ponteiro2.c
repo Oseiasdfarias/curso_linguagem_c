ff0da75a8676f588d99164de278ebedc2372b618
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-05-24 23:26:36 -0300

    atualização exercicio ponteiro de ponteiro 2

--- ponteiro/ponteiro_de_ponteiro2.c
+++ ponteiro/ponteiro_de_ponteiro2.c
@@ -8,23 +8,18 @@ int main(){
 	int *p = &x;
 	int **p2 = &p;
 
-	printf("\n");
 	// Imprime o Endereço de *p que p2 armazena
-	printf("End apontado por P2: %p\n", p2);
+	printf("\nEnd apontado por P2: %p\n", p2);
 	printf("End de P: %p\n", &p);
 
 	//Conteúdo de *p que é o endereço da variável x 	
-	printf("\n");
-	printf("Conteúdo de *p acessado via *p2: %p\n", *p2);
+	printf("\nConteúdo de *p acessado via *p2: %p\n", *p2);
 	printf("Conteúdo de *p: %p\n", p);
 	printf("Endereço de x: %p\n", &x);
 
 	// Conteúdo do endereço apontado por *p
-	printf("\n");
-	printf("Valor de x acessado via **p2: %d\n", **p2);
+	printf("\nValor de x acessado via **p2: %d\n", **p2);
 	printf("Valor de x: %d\n\n", x);
-
-	printf("\n");
 
 	return 0;
 
