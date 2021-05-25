e11304089fecaa115dffeb822eb9d7064262cf60
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-05-25 20:37:38 -0300

    atualização pasta ponteiros

--- ponteiro/arrays_de_ponteiros.c
+++ ponteiro/arrays_de_ponteiros.c
@@ -3,25 +3,26 @@
 int main(){
 /* Também podemos declarar um 
  * array de ponteiros */
-int *vet[5]; // array contendo 5 ponteiros
-int i, j;
 
-printf("\n");
-for (i = 0; i < 5; i++){
-	printf("%p\n", vet[i]);
+// array contendo 5 ponteiros
+	int *vet[5]; int i, j;
+	
+	printf("\n");
+	for (i = 0; i < 5; i++){
+		printf("%p\n", vet[i]);
 	
 	}
 
-printf("\n");
-int array[5] = {1, 2, 3, 4, 5};
-for (j = 0; j < 5; j++){
-	vet[j] = &array[j];
-	printf("vet valor: %d End: %p | ", *vet[j], vet[j]);
-	printf("array valor: %d End: %p\n", array[j], &array[j]);
+	printf("\n");
+	int array[5] = {1, 2, 3, 4, 5};
 
+	for (j = 0; j < 5; j++){
+		vet[j] = &array[j];
+		printf("vet valor: %d End: %p | ", *vet[j], vet[j]);
+		printf("array valor: %d End: %p\n", array[j], &array[j]);
 	}
 
-printf("\n");
-return 0;
+	printf("\n");
+	return 0;
 
 }
